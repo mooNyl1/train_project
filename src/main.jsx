0f5fa835eca4b531f376527f736a477585aed53c
Имиль Гафиатулин <moonyl1@MacBook-Air-Imil.local>
2025-04-08 17:42:49 +0300

    Тест2

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-
-import { init, miniApp } from '@telegram-apps/sdk';
-
-
-const initializeTelegramSDK = async () => {
-  try {
-    await init();
-
-
-    if (miniApp.ready.isAvailable()) {
-      await miniApp.ready();
-      console.log('Mini App готово');
-    }
-
-
-  } catch (error) {
-    console.error('Ошибка инициализации:', error);
-  }
-};
-
-
-initializeTelegramSDK();
-
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
